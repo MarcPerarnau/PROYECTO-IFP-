ac1b92849fb33c99e8800cae132d724c40f8f1c1
DevLab <151735878+MarcPerarnau@users.noreply.github.com>
2024-10-29 20:39:54 +0100

    Update Mensajes.sql

--- MySQL/Bases de Datos/Mensajes.sql
+++ MySQL/Bases de Datos/Mensajes.sql
@@ -1,6 +1,6 @@
 CREATE Table if not exists mensajes(
     id_mensaje int AUTO_INCREMENT PRIMARY KEY,
     mensaje TEXT not NULL,
-    matricula VARCHAR(10) NOT NULL,
+    matricula VARCHAR(70) NOT NULL,
     fecha timestamp default current_timestamp
 );
