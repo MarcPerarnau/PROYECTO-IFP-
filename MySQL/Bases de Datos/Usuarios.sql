6d503fde1cfc34cd844da0462f31046fa587ea42
micaelaoly <andreasandu04@gmail.com>
2024-10-29 17:47:44 +0100

    Update Usuarios.sql

--- MySQL/Bases de Datos/Usuarios.sql
+++ MySQL/Bases de Datos/Usuarios.sql
@@ -1,6 +1,10 @@
 create Table if not exists usuaris(
     email VARCHAR(100) PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
+    apellido1  VARCHAR(100) NOT NULL,
+    apellido2  VARCHAR(100) NULL,
     telefono VARCHAR(10) NOT NULL,
-    contrasena VARCHAR(27) NOT NULL
+    contrasena VARCHAR(27) NOT NULL,
+    fecha timestamp default current_timestamp,
+    img  VARCHAR(100) NOT NULL
 );
