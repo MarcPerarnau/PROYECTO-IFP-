07f6d3e5cd0aba6a4266e78ce019aa87b2c7eca2
micaelaoly <andreasandu04@gmail.com>
2024-10-29 17:45:50 +0100

    Update Mensajes.sql

--- MySQL/Bases de Datos/Mensajes.sql
+++ MySQL/Bases de Datos/Mensajes.sql
@@ -1,6 +1,7 @@
 CREATE Table if not exists mensajes(
     id_mensaje int AUTO_INCREMENT PRIMARY KEY,
-    texto TEXT not NULL,
+    mensaje TEXT not NULL,
     matricula VARCHAR(10) NOT NULL,
-    fecha timestamp default current_timestamp
+    fecha timestamp default current_timestamp,
+    
 );
